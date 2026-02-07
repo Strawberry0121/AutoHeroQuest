84db55006480129330330f34740d7b2b6612213c
Strawberry0121 <kokihigashi0121@icloud.com>
2026-02-07 15:41:10 +0900

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.model;
 
 import java.util.List;
 import java.util.Random;
@@ -7,41 +7,22 @@ public class Event {
     private String name;
     private String type;
     private int score;
-    private int damage;
-    private int heal;
     private List<String> actions;
     private Random random = new Random();
 
-    public Event(String name, String type, int score, int damage, int heal, List<String> actions) {
+    public Event(String name, String type, int score, List<String> actions) {
         this.name = name;
         this.type = type;
         this.score = score;
-        this.damage = damage;
-        this.heal = heal;
         this.actions = actions;
     }
 
     public String getName() { return name; }
     public String getType() { return type; }
     public int getScore() { return score; }
-    public int getDamage() { return damage; }
-    public int getHeal() { return heal; }
 
     public String getRandomAction() {
-        if (actions == null || actions.isEmpty()) return "";
+        if(actions == null || actions.isEmpty()) return "";
         return actions.get(random.nextInt(actions.size()));
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Event)) return false;
-        Event e = (Event)obj;
-        return name.equals(e.name) && type.equals(e.type);
-    }
-
-    @Override
-    public int hashCode() {
-        return name.hashCode() + type.hashCode();
     }
 }
